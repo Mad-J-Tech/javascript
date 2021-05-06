54c71c591e90b819db4c4a7bdfd11c994f76e86d
JIn Kimura <jellyfishman520@gmail.com>
2021-05-07 08:13:40 +0900

    2 FizzBuzz

--- js2/FizzBuzz.js
+++ js2/FizzBuzz.js
@@ -1,11 +1,16 @@
-const fizz = document.getElementById('fizz').value;
-const buzz = document.getElementById('buzz').value;
+function check() {
 
-for (let i = 1; i < 100; i++) {
-    if (i % fizz === 0 && i % buzz !== 0) {
-        let elem = document.createElement('p');
-        let num = document.createTextNode(i);
-        document.body.appendChild(elem);
-        elem.appendChild(num);
+    const fizz = parseInt(document.fizzbuzz.elements[0].value);
+    const buzz = parseInt(document.fizzbuzz.elements[1].value);
+
+    console.log(fizz);
+
+    for (let i = 1; i < 100; i++) {
+        if (i % fizz === 0 && i % buzz !== 0) {
+            let elem = document.createElement('p');
+            let num = document.createTextNode(i);
+            document.body.appendChild(elem);
+            elem.appendChild(num);
+        }
     }
 }
