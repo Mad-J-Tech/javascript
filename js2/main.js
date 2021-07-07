4255745c47191cbe956739daca77a63138f6e82e
JIn Kimura <jellyfishman520@gmail.com>
2021-07-08 06:33:45 +0900

    【Update】実行結果の上書き

--- js2/main.js
+++ js2/main.js
@@ -1,57 +1,65 @@
 function check() {
 
-    document.querySelector('p').remove();
+    const parentDiv = document.getElementById("parentDiv");
+    while (parentDiv.firstChild) {
+        parentDiv.removeChild(parentDiv.firstChild);
+    }
 
     const fizzStr = document.fizzbuzz.elements[0].value;
     const buzzStr = document.fizzbuzz.elements[1].value;
 
-    function toHalfWidth(str) {
+    function convertToHalfwith(str) {
         return str.replace(/[０-９]/g, function (s) {
             return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
         });
     }
 
-    function fizzbuzz(fizz, buzz) {
+    function showResult(fizz, buzz) {
         for (let i = 1; i < 100; i++) {
             if (i % fizz === 0 && i % buzz !== 0) {
                 let elem = document.createElement('p');
                 let num = document.createTextNode("Fizz " + i);
-                document.body.appendChild(elem);
+                let parentDiv = document.getElementById("parentDiv");
+                parentDiv.insertBefore(elem, null);
                 elem.appendChild(num);
             } else if (i % fizz !== 0 && i % buzz === 0) {
                 let elem = document.createElement('p');
                 let num = document.createTextNode("Buzz " + i);
-                document.body.appendChild(elem);
+                let parentDiv = document.getElementById("parentDiv");
+                parentDiv.insertBefore(elem, null);
                 elem.appendChild(num);
             } else if (i % fizz === 0 && i % buzz === 0) {
                 let elem = document.createElement('p');
                 let num = document.createTextNode("FizzBuzz " + i);
-                document.body.appendChild(elem);
+                let parentDiv = document.getElementById("parentDiv");
+                parentDiv.insertBefore(elem, null);
                 elem.appendChild(num);
             }
         }
     }
 
-    function error() {
+    function showError() {
         let elem = document.createElement('p');
         let text = document.createTextNode("整数を入力してください");
-        document.body.appendChild(text);
+        let parentDiv = document.getElementById("parentDiv");
+        parentDiv.insertBefore(elem, null);
+        elem.appendChild(text);
     }
 
     if (Number.isInteger(parseFloat(fizzStr)) && Number.isInteger(parseFloat(buzzStr))) {
         const fizz = fizzStr;
         const buzz = buzzStr;
-        fizzbuzz(fizz, buzz);
+        showResult(fizz, buzz);
     } else if (fizzStr.match(/[０-９]/) && buzzStr.match(/[0-9]/)) {
-        const fizz = toHalfWidth(fizzStr);
+        const fizz = convertToHalfwith(fizzStr);
         const buzz = buzzStr;
-        fizzbuzz(fizz, buzz);
+        showResult(fizz, buzz);
     } else if (buzzStr.match(/[０-９]/) && fizzStr.match(/[0-9]/)) {
         const fizz = fizzStr;
-        const buzz = toHalfWidth(buzzStr);
-        fizzbuzz(fizz, buzz);
+        const buzz = convertToHalfwith(buzzStr);
+        showResult(fizz, buzz);
     } else {
-        error();
+        showError();
     }
 
 }
