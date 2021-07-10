97d994dd30e44587308fa0ab2a9dc3e7a0d04530
JIn Kimura <jellyfishman520@gmail.com>
2021-07-10 10:35:01 +0900

    【Update】イベント処理の追加

--- js2/main.js
+++ js2/main.js
@@ -1,5 +1,45 @@
-function check() {
 
+function convertToHalfwith(str) {
+    return str.replace(/[０-９]/g, function (s) {
+        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    });
+}
+
+function showResult(fizz, buzz) {
+    for (let i = 1; i < 100; i++) {
+        if (i % fizz === 0 && i % buzz !== 0) {
+            let elem = document.createElement('p');
+            let num = document.createTextNode("Fizz " + i);
+            let parentDiv = document.getElementById("parentDiv");
+            parentDiv.insertBefore(elem, null);
+            elem.appendChild(num);
+        } else if (i % fizz !== 0 && i % buzz === 0) {
+            let elem = document.createElement('p');
+            let num = document.createTextNode("Buzz " + i);
+            let parentDiv = document.getElementById("parentDiv");
+            parentDiv.insertBefore(elem, null);
+            elem.appendChild(num);
+        } else if (i % fizz === 0 && i % buzz === 0) {
+            let elem = document.createElement('p');
+            let num = document.createTextNode("FizzBuzz " + i);
+            let parentDiv = document.getElementById("parentDiv");
+            parentDiv.insertBefore(elem, null);
+            elem.appendChild(num);
+        }
+    }
+}
+
+function showError() {
+    let elem = document.createElement('p');
+    let text = document.createTextNode("整数を入力してください");
+    let parentDiv = document.getElementById("parentDiv");
+    parentDiv.insertBefore(elem, null);
+    elem.appendChild(text);
+}
+
+const btn = document.getElementById("button");
+btn.addEventListener("click", () => {
+    
     const parentDiv = document.getElementById("parentDiv");
     while (parentDiv.firstChild) {
         parentDiv.removeChild(parentDiv.firstChild);
@@ -7,44 +47,6 @@ function check() {
 
     const fizzStr = document.fizzbuzz.elements[0].value;
     const buzzStr = document.fizzbuzz.elements[1].value;
-
-    function convertToHalfwith(str) {
-        return str.replace(/[０-９]/g, function (s) {
-            return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-        });
-    }
-
-    function showResult(fizz, buzz) {
-        for (let i = 1; i < 100; i++) {
-            if (i % fizz === 0 && i % buzz !== 0) {
-                let elem = document.createElement('p');
-                let num = document.createTextNode("Fizz " + i);
-                let parentDiv = document.getElementById("parentDiv");
-                parentDiv.insertBefore(elem, null);
-                elem.appendChild(num);
-            } else if (i % fizz !== 0 && i % buzz === 0) {
-                let elem = document.createElement('p');
-                let num = document.createTextNode("Buzz " + i);
-                let parentDiv = document.getElementById("parentDiv");
-                parentDiv.insertBefore(elem, null);
-                elem.appendChild(num);
-            } else if (i % fizz === 0 && i % buzz === 0) {
-                let elem = document.createElement('p');
-                let num = document.createTextNode("FizzBuzz " + i);
-                let parentDiv = document.getElementById("parentDiv");
-                parentDiv.insertBefore(elem, null);
-                elem.appendChild(num);
-            }
-        }
-    }
-
-    function showError() {
-        let elem = document.createElement('p');
-        let text = document.createTextNode("整数を入力してください");
-        let parentDiv = document.getElementById("parentDiv");
-        parentDiv.insertBefore(elem, null);
-        elem.appendChild(text);
-    }
 
     if (Number.isInteger(parseFloat(fizzStr)) && Number.isInteger(parseFloat(buzzStr))) {
         const fizz = fizzStr;
@@ -61,5 +63,7 @@ function check() {
     } else {
         showError();
     }
+});
 
-}
+
+
