e31eae3ff58168da53079159b46195d2f27ff645
JIn Kimura <jellyfishman520@gmail.com>
2021-07-13 07:04:16 +0900

    【Update】タスク追加方法の修正

--- js3/main.js
+++ js3/main.js
@@ -1,3 +1,4 @@
+/*
 const table = document.getElementById('table');
 
 const btn = document.getElementById('btn');
@@ -26,5 +27,46 @@ btn.addEventListener('click', () => {
     delbtn.type = 'button';
     delbtn.value = '削除';
     newCell.appendChild(delbtn);
-
+    
 });
+*/
+
+const table = document.getElementById('table');
+
+const btn = document.getElementById('btn');
+
+let todos = [];
+
+btn.addEventListener('click', () => {
+    //テーブルの削除処理
+
+    const textbox = document.getElementById('comment');
+    const comment = textbox.value;
+    let todo = { task: comment, status: "作業中" };
+    todos.push(todo);
+
+    for (i = 0; i < todos.length; i++) {
+        const newRow = table.insertRow();
+        const index = i;
+        let newCell = newRow.insertCell();
+        let newText = document.createTextNode(index);
+        newCell.appendChild(newText);
+
+        newCell = newRow.insertCell();
+        newText = document.createTextNode(todos[i].task);
+        newCell.appendChild(newText);
+
+        newCell = newRow.insertCell();
+        const statbtn = document.createElement('input');
+        statbtn.type = 'button';
+        statbtn.value = todos[i].status;
+        newCell.appendChild(statbtn);
+
+        newCell = newRow.insertCell();
+        const delbtn = document.createElement('input');
+        delbtn.type = 'button';
+        delbtn.value = '削除';
+        newCell.appendChild(delbtn);
+    }
+});
+
