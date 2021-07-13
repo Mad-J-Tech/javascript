ff1dff07ede006e57170b73fdfe5b4dfc1367d11
JIn Kimura <jellyfishman520@gmail.com>
2021-07-14 07:05:03 +0900

    【Update】タスク追加方法の修正、削除機能の追加

--- js3/main.js
+++ js3/main.js
@@ -1,33 +1,40 @@
+
 /*
-const table = document.getElementById('table');
-
-const btn = document.getElementById('btn');
-
 btn.addEventListener('click', () => {
-    const newRow = table.insertRow();
-    const index = newRow.rowIndex - 1;
-    let newCell = newRow.insertCell();
-    let newText = document.createTextNode(index);
-    newCell.appendChild(newText);
-
+    //テーブルの削除処理
+    while (table.childNodes[1]) {
+        table.removeChild(table.childNodes[1]);
+    }
+    
     const textbox = document.getElementById('comment');
     const comment = textbox.value;
-    newCell = newRow.insertCell();
-    newText = document.createTextNode(comment);
-    newCell.appendChild(newText);
-
-    newCell = newRow.insertCell();
-    const statbtn = document.createElement('input');
-    statbtn.type = 'button';
-    statbtn.value = '作業中';
-    newCell.appendChild(statbtn);
-
-    newCell = newRow.insertCell();
-    const delbtn = document.createElement('input');
-    delbtn.type = 'button';
-    delbtn.value = '削除';
-    newCell.appendChild(delbtn);
+    let todo = { task: comment, status: "作業中" };
+    todos.push(todo);
     
+    for (i = 0; i < todos.length; i++) {
+        const newRow = table.insertRow();
+        const index = i;
+        let newCell = newRow.insertCell();
+        let newText = document.createTextNode(index);
+        newCell.appendChild(newText);
+        
+        newCell = newRow.insertCell();
+        newText = document.createTextNode(todos[i].task);
+        newCell.appendChild(newText);
+        
+        newCell = newRow.insertCell();
+        const statbtn = document.createElement('input');
+        statbtn.type = 'button';
+        statbtn.value = todos[i].status;
+        newCell.appendChild(statbtn);
+        
+        newCell = newRow.insertCell();
+        const delbtn = document.createElement('input');
+        delbtn.type = 'button';
+        delbtn.value = '削除';
+        delbtn.classList.add('trash');
+        newCell.appendChild(delbtn);
+    }
 });
 */
 
@@ -37,13 +44,10 @@ const btn = document.getElementById('btn');
 
 let todos = [];
 
-btn.addEventListener('click', () => {
-    //テーブルの削除処理
-
-    const textbox = document.getElementById('comment');
-    const comment = textbox.value;
-    let todo = { task: comment, status: "作業中" };
-    todos.push(todo);
+function showTasks() {
+    while (table.childNodes[1]) {
+        table.removeChild(table.childNodes[1]);
+    }
 
     for (i = 0; i < todos.length; i++) {
         const newRow = table.insertRow();
@@ -66,7 +70,27 @@ btn.addEventListener('click', () => {
         const delbtn = document.createElement('input');
         delbtn.type = 'button';
         delbtn.value = '削除';
+        delbtn.classList.add('trash');
         newCell.appendChild(delbtn);
     }
-});
+
+}
+
+function addTask() {
+    const textbox = document.getElementById('comment');
+    const comment = textbox.value;
+    let todo = { task: comment, status: "作業中" };
+    todos.push(todo);
+}
+
+function deleteTask() {
+    const delbtns = document.getElementsByClassName('trash');
+
+    for (i = 0; i < delbtns.length; i++) {
+        delbtns[i].addEventListener('click', () => {
+            todos.splice(i, 1);
+        });
+    }
+}
+
 
